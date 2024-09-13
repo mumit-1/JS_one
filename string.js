c81dbd5329652f2f7f58a85eefe6f442ddf59436
mumit-1 <mumitkhan85@gmail.com>
2024-09-14 00:11:29 +0600

    added string

---
+++ string.js
@@ -0,0 +1,12 @@
+const thana = `dhaka`;
+const district = 'sadpur'
+const bivag = new String('demra');
+console.log(thana);
+
+console.log(bivag);
+
+console.log(district.length);
+console.log(district[1]);
+
+
+// string kinda works like array but not like fully array . in array we can change the value by calling his index but in array we can't do that but we can do like calling index and see the value or its lenght like array 
