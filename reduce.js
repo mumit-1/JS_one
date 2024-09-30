ff0d282fac1c70c39d9efe312144d73e851792c3
mumit-1 <mumitkhan85@gmail.com>
2024-09-30 12:36:30 +0600

    class

--- reduce.js
+++ reduce.js
@@ -1,4 +1,4 @@
 const array = [1, 2, 3, 4, 56, 7, 7, 4];
 
-const result = array.reduce((p, c) => p + c, 0); //  it gives the sum of numbers in the array
+const result = array.reduce((p, c) => p + c, 0); //  it gives the sum of numbers in the array . "c" stands for new value and "p" stands for the storing the sum.
 console.log(result);
